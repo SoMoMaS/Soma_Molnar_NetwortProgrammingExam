b6d60bcc7439fd008406ad747ef154f8511e6b5f
soma.matyas.molnar@gmail.com <soma.matyas.molnar@gmail.com>
2020-11-02 14:31:50 +0100

    Same host configuration on the client implemented.

--- RandomChar/RandomCharService/Client/ConfigurationSetter.cs
+++ RandomChar/RandomCharService/Client/ConfigurationSetter.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RandomCharServiceInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Client
 {
-    class ConfigurationSetter
+    public class ConfigurationSetter
     {
+        private IHost myHost;
+        public async Task ConfigureHost(string[] arguments)
+        {
+            this.myHost = new HostBuilder()
+                .ConfigureServices(confServ =>
+                {
+                    confServ.AddTransient<IArgumentValidatorService, ArgumentValidatorService>();
+                    confServ.AddTransient<Runner>();
+                })
+                .ConfigureAppConfiguration(appConf =>
+                {
+                    appConf.AddCommandLine(arguments);
+                })
+                .ConfigureLogging(loggConf =>
+                {
+                    loggConf.AddDebug();
+                    loggConf.AddConsole();
+                }).Build();
+
+            this.myHost.Start();
+
+            var runner = this.myHost.Services.GetRequiredService<Runner>();
+
+            await runner.StartAppAsync();
+
+
+        }
     }
 }
